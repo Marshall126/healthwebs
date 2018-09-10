daa05a538261d552d90757b728920ca63e3251ea
Henry Madden <rfmaddenh@gmail.com>
2018-09-09 20:21:25 -0500

    This is the project maybe

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-    
+    let lis=["#Antibiotics", '#Agriculture', "#Cleaning", '#Prevention', '#Sources'];
+    $("ul#navu li").click(function(){
+        for(i=0; i<lis.length; i++){
+            $(`${lis[i]}`).css({
+                'position':'absolute',
+                'visibility':'hidden'
+            });
+        };
+        $(`#${$(this).data('para')}`).css({
+            'position': 'relative',
+            'visibility': 'visible',
+        });
+    });
 });
